1ee05f52613d2b8a0f2ce92c4d914faa349d0d01
cwalters22 <71747723+cwalters22@users.noreply.github.com>
2020-10-27 00:35:52 -0700

    update

---
+++ processing programs/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(600, 400);
+    background(200);
+    ellipse(mouseX, mouseY, 100, 150);
+    fill(0, 234, 234)
+  }
+  
+  function draw() {
+    variableEllipse(mouseX,mouseY,pmouseX,pmouseY); 
+  }
+  
+  function mousePressed() {
+    if (mouseIsPressed) {
+    fill(153, 0, 153);
+    }
+  }
+  
+  
+  function variableEllipse(x,y,px,py) {
+    let speed = abs(x-px) +abs(y-py);
+    stroke(speed);
+    ellipse(x,y,speed,speed)
+  } 
+  
